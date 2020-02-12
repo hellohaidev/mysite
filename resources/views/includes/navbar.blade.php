741cbefff5e11162a82fb4690f59601a7caee67d
adirahman <com.adirahman@gmail.com>
2020-02-12 09:21:58 +0700

    add menu contact

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -9,6 +9,9 @@
         <li class="nav-item active">
           <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact <span class="sr-only">(current)</span></a>
+        </li>
       </ul>
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search">
