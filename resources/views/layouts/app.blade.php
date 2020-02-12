bac2d2de1ddaae1a6e94da9e9eef168873fe4cd2
adirahman <com.adirahman@gmail.com>
2020-02-12 10:02:50 +0700

    penmabahan link font google

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Site</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300i,400&display=swap" rel="stylesheet">
     @include('includes.style')
 </head>
 <body>
